003ef9744b9403a21b141f8cae11070eb29d027b
Ady Rose <Rose.Adrian@outlook.com>
2020-06-26 12:32:19 +0100

    tabs and sliders now part of the Roseworx Core

--- js/roseworx.js
+++ js/roseworx.js
@@ -1,5 +1,7 @@
 import rwxForms from './components/rwxForms';
 import rwxTables from './components/rwxTables';
+import rwxTabs from './components/rwxTabs';
+import rwxSliders from './components/rwxSliders'
 
-export default {forms:rwxForms}; 
+export default {forms:rwxForms, sliders:rwxSliders, tabs:rwxTabs}; 
 //Forms has the custom submit method users can use

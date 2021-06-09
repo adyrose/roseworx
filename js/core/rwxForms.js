3a7142b6d8f19638e88beca4b9a2d099e7174a1d
Ady Rose <Rose.Adrian@outlook.com>
2021-06-09 20:53:17 +0100

    updates and fixes to forms from implementation

--- js/core/rwxForms.js
+++ js/core/rwxForms.js
@@ -23,29 +23,45 @@ class rwxForm extends rwxComponent {
 	constructor(el)
 	{
 		super({element: el})
-		const submitButton = this.el.querySelector("button[type='submit']");
+		this.submitButton = this.el.querySelector("button[type='submit']");
 
 		this.validEls = {};
+
+		this.events = []
 
 		this.getInputs().map((el)=>{
 			this.determineValid(el);
 			this.touched(el);
 
-			el.addEventListener('blur', (e)=>{
+			const blurev = (e) =>{
 				el.type == "checkbox" ? this.addValidClass(this.validCheckbox(el, e.target.checked), el) : this.addValidClass(this.validInput(el, e.target.value), el);
-			});
+			}
+
+			el.addEventListener('blur', blurev);
 
 			const validEvent = (el.type=="checkbox" || el.tagName=="SELECT" || el.type=="radio") ? 'change' : 'keyup';
-			el.addEventListener(validEvent, (e)=>{
+
+			const validEventEvent = (e) => {
 				this.determineValid(el);
-				if(submitButton)submitButton.disabled = !this.isFormValid();
-			});
+				if(this.submitButton)this.submitButton.disabled = !this.isFormValid();			
+			}
+
+			el.addEventListener(validEvent, validEventEvent);
+			this.events.push([{
+					type: 'blur',
+					ev: blurev
+				},
+				{
+					type: validEvent,
+					ev: validEventEvent
+				}
+			])
 			return;
 		});
 
-		if(submitButton)submitButton.disabled = !this.isFormValid();
+		if(this.submitButton)this.submitButton.disabled = !this.isFormValid();
 
-		this.el.addEventListener('submit', (ev)=>{
+		this.submitFn = (ev) => {
 			if(!this.isFormValid())
 			{
 				ev.preventDefault();
@@ -63,6 +79,21 @@ class rwxForm extends rwxComponent {
 				})
 				this.customSubmit(finalObject);
 			}
+		}
+		this.submitFn = this.submitFn.bind(this);
+		this.el.addEventListener('submit', this.submitFn)
+	}
+
+	cleanUp()
+	{
+		this.el.removeEventListener('submit', this.submitFn);
+		this.getInputs().map((inp, i)=>{
+			this.events[i].map((e)=>inp.removeEventListener(e.type, e.ev));
+			inp.parentNode.classList.remove('valid');
+			inp.parentNode.classList.remove('invalid');
+			inp.parentNode.classList.remove('touched');
+			this.submitButton.disabled = false;
+			return false;
 		})
 	}
 
